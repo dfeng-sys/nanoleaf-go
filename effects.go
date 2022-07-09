684b6bb5b70b27e7951ac173124bfa4eb88dd30d
Da F <dfeng.xix@gmail.com>
2022-07-09 16:46:55 -0400

    added several effect functions

--- effects.go
+++ effects.go
@@ -14,11 +14,33 @@ type NanoEffects struct {
 
 // EffectData effects data
 type EffectData struct {
-	Loop    bool   `json:"loop"`
-	Name    string `json:"animName"`
-	Type    string `json:"animType"`
-	Version string `json:"version"`
-	Data    string `json:"animData"`
+	Command              string         `json:"command,omitempty"`
+	Loop                 bool           `json:"loop,omitempty"`
+	Name                 string         `json:"animName,omitempty"`
+	Type                 string         `json:"animType,omitempty"`
+	Version              string         `json:"version,omitempty"`
+	Data                 string         `json:"animData,omitempty"`
+	ColorType            string         `json:"colorType,omitempty"`
+	Palette              []PaletteColor `json:"palette,omitempty"`
+	PluginType           string         `json:"pluginType,omitempty"`
+	PluginUuid           string         `json:"pluginUuid,omitempty"`
+	PluginOptions        []PluginOption `json:"pluginOptions,omitempty"`
+	OverlayPalette       []PaletteColor `json:"overlayPalette,omitempty"`
+	OverlayColorType     string         `json:"overlayColorType,omitempty"`
+	HasOverlay           bool           `json:"hasOverlay,omitempty"`
+	LogicalPanelsEnabled bool           `json:"logicalPanelsEnabled,omitempty"`
+}
+
+type PaletteColor struct {
+	Hue         int     `json:"hue"`
+	Saturation  int     `json:"saturation"`
+	Brightness  int     `json:"brightness"`
+	Probability float64 `json:"probability"`
+}
+
+type PluginOption struct {
+	Name  string      `json:"name"`
+	Value interface{} `json:"value"` // int, float, bool, or string
 }
 
 // newNanoEffects returns a new NanoEffects instance
@@ -140,6 +162,133 @@ func (e *NanoEffects) GetEffectData(effect string) (EffectData, error) {
 	return data, nil
 }
 
+// returns data of all effects on the device
+func (e *NanoEffects) GetAllEffectData() ([]EffectData, error) {
+
+	animations := struct {
+		Animations []EffectData `json:"animations"`
+	}{}
+	body := jsonPayload{
+		"write": jsonPayload{
+			"command": "requestAll",
+		},
+	}
+	resp, err := e.nano.client.R().SetHeader("Content-Type", "application/json").SetBody(body).Put(e.endpoint)
+
+	if err != nil {
+		return animations.Animations, err
+	}
+
+	if resp.StatusCode() == http.StatusUnauthorized {
+		return animations.Animations, ErrUnauthorized
+	}
+
+	if resp.StatusCode() == http.StatusNotFound {
+		return animations.Animations, ErrEffectNotFound
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		return animations.Animations, ErrUnexpectedResponse
+	}
+
+	if err := json.Unmarshal(resp.Body(), &animations); err != nil {
+		fmt.Println(err)
+		return animations.Animations, ErrParsingJSON
+	}
+
+	return animations.Animations, nil
+}
+
+// /effects (rename)
+func (e *NanoEffects) RenameEffect(animName, newName string) error {
+
+	body := jsonPayload{
+		"write": jsonPayload{
+			"command":  "rename",
+			"animName": animName,
+			"newName":  newName,
+		},
+	}
+	resp, err := e.nano.client.R().SetHeader("Content-Type", "application/json").SetBody(body).Put(e.endpoint)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() == http.StatusUnauthorized {
+		return ErrUnauthorized
+	}
+
+	if resp.StatusCode() == http.StatusNotFound {
+		return ErrEffectNotFound
+	}
+
+	if resp.StatusCode() != http.StatusNoContent {
+		return ErrUnexpectedResponse
+	}
+
+	return nil
+}
+
+// /effects (add/update)
+func (e *NanoEffects) AddEffect(data EffectData, animName string) error {
+
+	data.Command = "add"
+	data.Name = animName
+	body := jsonPayload{
+		"write": data,
+	}
+	resp, err := e.nano.client.R().SetHeader("Content-Type", "application/json").SetBody(body).Put(e.endpoint)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() == http.StatusUnauthorized {
+		return ErrUnauthorized
+	}
+
+	if resp.StatusCode() == http.StatusNotFound {
+		return ErrEffectNotFound
+	}
+
+	if resp.StatusCode() != http.StatusNoContent {
+		return ErrUnexpectedResponse
+	}
+
+	return nil
+}
+
+// deletes the given effect
+func (e *NanoEffects) DeleteEffect(animName string) error {
+
+	body := jsonPayload{
+		"write": jsonPayload{
+			"command":  "delete",
+			"animName": animName,
+		},
+	}
+	resp, err := e.nano.client.R().SetHeader("Content-Type", "application/json").SetBody(body).Put(e.endpoint)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() == http.StatusUnauthorized {
+		return ErrUnauthorized
+	}
+
+	if resp.StatusCode() == http.StatusNotFound {
+		return ErrEffectNotFound
+	}
+
+	if resp.StatusCode() != http.StatusNoContent {
+		return ErrUnexpectedResponse
+	}
+
+	return nil
+}
+
 // WriteRaw writes the raw command (outcome will depend on your body because the nanoleaf api is not well designed)
 func (e *NanoEffects) WriteRaw(body jsonPayload) error {
 	resp, err := e.nano.client.R().SetHeader("Content-Type", "application/json").SetBody(body).Put(e.endpoint)
@@ -159,14 +308,28 @@ func (e *NanoEffects) WriteRaw(body jsonPayload) error {
 	return nil
 }
 
-// Temp displays effect described given animData temporarily
-func (e *NanoEffects) Temp(data string, loop bool) error {
+// displays effect with the given animData, looping if specified
+func (e *NanoEffects) Display(data string, loop bool) error {
 	body := jsonPayload{
 		"write": jsonPayload{
-			"command":  "display",
-			"animType": "custom",
-			"animData": data,
-			"loop":     loop,
+			"command":   "display",
+			"animType":  "custom",
+			"colorType": "RGB",
+			"animData":  data,
+			"loop":      loop,
+		},
+	}
+
+	return e.WriteRaw(body)
+}
+
+// temporarily displays the given effect for the specified duration
+func (e *NanoEffects) DisplayTemp(animName string, duration int) error {
+	body := jsonPayload{
+		"write": jsonPayload{
+			"command":  "displayTemp",
+			"duration": duration,
+			"animName": animName,
 		},
 	}
 
